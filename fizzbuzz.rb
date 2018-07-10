a6326b07c43eff203cb67b706f78c8ceacb81fcd
jesskwong <jesskwong9@gmail.com>
2018-07-10 19:41:16 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,17 @@
+def fizzbuzz(int)
+
+if int % 3 == 0 && int % 5 == 0 
+  "FizzBuzz" 
+
+elsif int % 3 == 0 
+  "Fizz"
+
+elsif int % 5 == 0 
+  "Buzz"
+end 
+
+end
+
+puts fizzbuzz(6)
+fizzbuzz(4)
+fizzbuzz(6)
